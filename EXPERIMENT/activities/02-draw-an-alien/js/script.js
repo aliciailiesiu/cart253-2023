1c31b14d2608254451ab1f68868fdabb0865848f
aliciailiesiu <aliciailiesiu@outlook.com>
2023-09-13 21:39:02 -0700

    activity 02: started to draw an alien activity

--- EXPERIMENT/activities/02-draw-an-alien/js/script.js
+++ EXPERIMENT/activities/02-draw-an-alien/js/script.js
@@ -20,11 +20,6 @@ function preload() {
  * Description of setup
 */
 function setup() {
-    createCanvas(500,500);
-background(255,200,200);
-fill(0);
-ellipseMode(CENTER);
-ellipse(250,250,200,200);
 
 }
 
