3383e7d40be85e9603a82e7a0a54985a70692b5f
aliciailiesiu <aliciailiesiu@outlook.com>
2023-09-21 12:26:04 -0700

    worked on moving picture activity

--- EXPERIMENT/activities/03-moving-pictures/js/script.js
+++ EXPERIMENT/activities/03-moving-pictures/js/script.js
@@ -23,7 +23,9 @@ let bg = {
 let circle1 = {
     x:0,
     y:250,
+    speed:1,
     size:100,
+    growthRate:1,
     fill:255,
     alpha:200,
 
@@ -32,6 +34,8 @@ let circle2 = {
     x:500,
     y:250,
     size:75,
+    sizeRatio:0.75,
+    speed:-1,
     fill:255,
     alpha:200
 }
@@ -44,14 +48,28 @@ createCanvas(500,500);
 noStroke();
 }
 
-
 /**
  * Description of draw()
 */
 function draw() {
     //background//
 background(bg.red,bg.green,bg.blue);
-bg.red += 1;
+bg.red = map(circle1.size,100,width,0,255);
+
+//circle 1//
+circle1.x = circle1.x + circle1.speed ;
+circle1.size = circle1.growthRate;
+circle1.size= constrain(circle1.size,0,width);
+circle1.x = constrain(circle1.x,0,width/2);
 fill(circle1.fill,circle1.alpha);
-ellipse(circle1.x,circle1.y,circle1.size)
+ellipse(circle1.x,circle1.y,circle1.size);
+
+
+
+//circle 2//
+circle2.x = circle2.x + circle2.speed;
+circle2.x = constrain(circle2.x,width/2,width);
+circle2.size = circle1.size * circle2.sizeRatio;
+fill(circle2.fill,circle2.alpha);
+ellipse(circle2.x,circle2.y,circle2.size);
 }
