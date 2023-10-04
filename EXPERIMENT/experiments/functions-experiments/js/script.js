901a94e7c3b27415e78733e0388ded130fdde773
aliciailiesiu <aliciailiesiu@outlook.com>
2023-10-04 15:10:06 -0700

    worked on function experiment

--- EXPERIMENT/experiments/functions-experiments/js/script.js
+++ EXPERIMENT/experiments/functions-experiments/js/script.js
@@ -6,27 +6,104 @@
  * and this description to match your project!
  */
 
-"use strict";
+//"use strict";
 
-/**
- * Description of preload
-*/
-function preload() {
-
+let circle = {
+  x: 250,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 2
 }
 
-
-/**
- * Description of setup
-*/
 function setup() {
+  createCanvas(500, 500);
 
+  // Linear movement down and to the right
+  circle.vx = circle.speed;
+  circle.vy = circle.speed;
 }
 
-
-/**
- * Description of draw()
-*/
 function draw() {
+  background(0);
 
+ let dx = circle.x - mouseX;
+ let dy = circle.y - mouseY;
+
+if (dx < 0) {
+  circle.vx = -circle.speed;
 }
+else if ( dx > 0) {
+  circle.vx = circle.speed;
+}
+
+if (dy < 0) {
+  circle.vy = -circle.speed;
+}
+else if ( dy > 0) {
+  circle.vy = circle.speed;
+}
+
+
+
+
+  circle.x = circle.x + circle.vx;
+  circle.y = circle.y + circle.vy;
+
+  ellipse(circle.x, circle.y, circle.size);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function setup() {
+    //createCanvas(500,500);
+
+    
+ // }
+  
+  //function draw() {
+   // background(0);
+
+   // let first = toMeters(39);
+    //console.log('39 inches is ${first} in meters');
+
+
+    //let second = toMeters(78);
+    //console.log('78 inches is ${second} in meters');
+
+  //}
+
+  //function toMeters(inches) {
+    //let meters = inches / 39;
+   // return meters;
+//}
+
+
